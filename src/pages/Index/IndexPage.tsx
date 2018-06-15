0b47cb3362a56168ddbe301181f4182e77541842
Honorwalk <daniel@accesssipd.com>
2018-06-15 12:34:41 -0700

    added background image. added inverted logo w/text

--- src/pages/Index/IndexPage.tsx
+++ src/pages/Index/IndexPage.tsx
@@ -11,30 +11,30 @@ import { Image } from 'uikit-react';
 import { Dark } from 'uikit-react';
 import { Link } from 'uikit-react';
 import { Icon } from 'uikit-react';
+import { Cover } from 'uikit-react';
 
 export class IndexPage extends React.Component {
     render() {
         return (
-            <Flex direction="column">
-                <Light>
-                    <Section color="secondary" padding="large">
-                        <Flex alignment="center">
-                            <object width="300" height="300" data="src/images/uikit-react.svg" />
-                        </Flex>
-                        <Flex alignment="center">
-                            <h1>UIkit React</h1>
-                        </Flex>
-                        <Flex alignment="center">
-                            <Button size="large" color="">Get Started</Button>
-                        </Flex>
-                    <Section padding="small">
-                        <Flex alignment="center">
-                            <h4> Use Uikit with React, a powerfully simple framwork </h4>
-                        </Flex>
-                    </Section>
-                    </Section>
+            <Flex direction="column"  >
+                <Light >
+                <Container size="large" type="image" style={{ backgroundImage: 'url("src/images/background.svg")', width: "expand", height:"100%"}}>
+                            <Flex alignment="center">
+                                <object width="300" height="300" data="src/images/uikit-react.svg" />
+                            </Flex>
+                            <Flex alignment="center">
+                                <h1>UIkit React</h1>
+                            </Flex>
+                            <Flex alignment="center">
+                                <Button size="large" color="">Get Started</Button>
+                            </Flex>
+                            <Section padding="small">
+                                <Flex alignment="center">
+                                    <h4> Use Uikit with React, a powerfully simple framework </h4>
+                                </Flex>
+                            </Section>
+                        </Container>
                 </Light>
-  
             </Flex>
         );
     }
