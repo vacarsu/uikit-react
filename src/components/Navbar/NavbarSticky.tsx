44caed58ff9652e207ceb0d47afa7702a22629f8
Honorwalk <daniel@accesssipd.com>
2018-06-18 00:18:47 -0700

    Added active anfd inactive event tags to navbar sticky
    WARNING I did not npm run build:prod. I cannot do so on my machine. This still has to be done. and i did not publish either.

--- src/components/Navbar/NavbarSticky.tsx
+++ src/components/Navbar/NavbarSticky.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 
 declare interface props {
+    id?: string;
     options?: string;
+    active?: Function;
+    inactive?: Function;
 }
 
 export class NavbarSticky extends React.Component<props, any> {
+    componentDidMount(){   
+        if(this.props.active){
+            this.validateIdProp();
+            UIkit.util.on(this.props.id, 'active', this.props.active);
+        }
+        if(this.props.inactive){
+            this.validateIdProp();
+            UIkit.util.on(this.props.id, 'active', this.props.inactive);
+        }
+        }
+ 
     render() {
         return (
             <div uk-sticky={`
@@ -15,5 +29,10 @@ export class NavbarSticky extends React.Component<props, any> {
                 {this.props.children}
             </div>
         );
+    }
+    private validateIdProp() {
+        if (!this.props.id) {
+            console.error("ID property is required to register to Navbar Sticky events");
+        }
     }
 }
