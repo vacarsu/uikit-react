44029398764c29b0355c7c7dd2864ab784754420
Arthur <nesterenko19@gmail.com>
2018-07-18 22:24:22 +0300

    add storybook

---
+++ stories/Button.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from './../src/components/Button/Button';
+import { wInfo } from '../.storybook/utils';
+
+storiesOf( 'Components/Button', module ).addWithJSX(
+    'basic Button',
+    wInfo( `
+
+  ### Notes
+
+  This is a button
+
+  ### Usage
+  ~~~js
+  <Button
+    type='primary'
+ 
+    onClick={() => alert('hello there')}
+  >Click</Button>
+  ~~~`,
+    )( () => (
+        <Button
+            size='small'
+        >Click</Button>
+    ) ),
+);
