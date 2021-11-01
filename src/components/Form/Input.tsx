0fe8ccc1668e57095c1d3ca793a53103754e826c
VenusTalent <93226408+VenusTalent@users.noreply.github.com>
2021-11-02 00:49:16 +0900

    Update Input.tsx
    It's difficult to cooperate with other css styles.
    I suggest the rename "value" to "checked" as the original one.

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -13,7 +13,7 @@ export class Input extends React.Component<FormInputProps, any> {
         className={this.setClassNames()}
         type="text"
         name={this.props.name ? this.props.name : null}
-        value={this.props.value}
+        checked={this.props.value}
         onChange={this.props.onChange}
         onBlur={this.props.onBlur}
         onFocus={this.props.onFocus}
